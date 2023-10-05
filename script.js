ecf73eb0f84df5b9b7e480d228adbfc542a0ba3e
JVPCoder <doublecanal@gmail.com>
2023-10-05 18:36:17 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+function carregar() {
+    var msg = document.querySelector('div#msg')
+    var img = document.querySelector('img#imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas`
+    
+    if(hora >= 0 && hora < 12){
+        img.src = 'manha.png'
+    }else if(hora >= 12 && hora < 18){
+        img.src = 'tarde.png'
+        document.body.style.background = '#DFAB7C'
+    }else{
+        img.src = 'noite.png'
+        document.body.style.background = '#3C3A48'
+    }
+}
